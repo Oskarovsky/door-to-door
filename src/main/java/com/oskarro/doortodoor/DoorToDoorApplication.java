474db367ab1c8419e3f799ccb1f86005c8407716
Oskarovsky <oskar.slyk@gmail.com>
2018-11-30 23:15:04 +0100

    Initial Project Add

---
+++ src/main/java/com/oskarro/doortodoor/DoorToDoorApplication.java
@@ -0,0 +1,12 @@
+package com.oskarro.doortodoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorToDoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorToDoorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/oskarro/doortodoor/DoorToDoorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oskarro.doortodoor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoorToDoorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
