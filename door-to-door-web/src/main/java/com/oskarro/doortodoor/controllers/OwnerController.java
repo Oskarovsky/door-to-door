51929747953237ccafddfd3dd17b82142e53e1e7
Oskarovsky <oskar.slyk@gmail.com>
2018-12-03 17:46:43 +0100

    Adding Owners List and updated controller. Closes #16

--- door-to-door-web/src/main/java/com/oskarro/doortodoor/controllers/OwnerController.java
+++ door-to-door-web/src/main/java/com/oskarro/doortodoor/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.oskarro.doortodoor.controllers;
 
+import com.oskarro.doortodoor.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // injected OwnerService via Controller (we don't need call out Autowired >4.2)
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
