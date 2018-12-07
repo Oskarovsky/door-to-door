dd11cf81b773e28c273d09bb2f23cebfb9fa3cb1
Oskarovsky <oskar.slyk@gmail.com>
2018-12-07 18:59:36 +0100

    Updated base entity with JPA annotations. Closes #35

--- door-to-door-data/src/main/java/com/oskarro/doortodoor/model/BaseEntity.java
+++ door-to-door-data/src/main/java/com/oskarro/doortodoor/model/BaseEntity.java
@@ -1,10 +1,19 @@
 package com.oskarro.doortodoor.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // this established this as a base class to JPA
 public class BaseEntity implements Serializable {
 
     // we can use also long type, but Long could be null, while long not.
+    // @Id this annotation tells JPA that this is the ID value
+    // @GeneratedValue [IDENTITY] - this going to provide us the identity value
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
