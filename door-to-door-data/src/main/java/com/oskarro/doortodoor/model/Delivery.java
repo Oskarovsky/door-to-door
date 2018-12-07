d957b2c97029f1c03eeed42d2d512619b23e6861
Oskarovsky <oskar.slyk@gmail.com>
2018-12-07 19:41:07 +0100

    Added delivery entity. Closes #38

--- door-to-door-data/src/main/java/com/oskarro/doortodoor/model/Delivery.java
+++ door-to-door-data/src/main/java/com/oskarro/doortodoor/model/Delivery.java
@@ -1,13 +1,27 @@
 package com.oskarro.doortodoor.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "deliveries")
 public class Delivery extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    @OneToOne
+    @JoinColumn(name = "courier_id")
     private Courier courier;
+
+    @Column(name = "cost")
     private String cost;
 
     public LocalDate getDate() {
