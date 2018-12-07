c37154323dd43f7a22b2e8b92e427491c47448f6
Oskarovsky <oskar.slyk@gmail.com>
2018-12-07 19:29:00 +0100

    Updated couriers to use JPA. Closes #37

--- door-to-door-data/src/main/java/com/oskarro/doortodoor/model/Courier.java
+++ door-to-door-data/src/main/java/com/oskarro/doortodoor/model/Courier.java
@@ -1,12 +1,24 @@
 package com.oskarro.doortodoor.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "couriers")
 public class Courier extends Person {
 
+    // JPA is going to try load everything all at once
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "courier_specialities",
+            joinColumns = @JoinColumn(name = "courier_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
+
+    @Column(name = "equipment")
     private String equipment;
+
+    @Column(name = "company")
     private String company;
 
     public String getEquipment() {
